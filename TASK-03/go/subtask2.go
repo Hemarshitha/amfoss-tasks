7dfbc1dceb0387b6e6fbfe9514af8e439821e6b6
Hemarshitha <hemarshitha2006@gmail.com>
2024-08-23 13:23:45 +0530

    added g0.2

---
+++ TASK-03/go/subtask2.go
@@ -0,0 +1,15 @@
+package main
+import (
+    "io/ioutil"
+    "log"
+)
+func main() {
+    data, err := ioutil.ReadFile("input.txt")
+    if err != nil {
+        log.Fatal(err)
+    }
+    err = ioutil.WriteFile("output.txt", data, 0644)
+    if err != nil {
+        log.Fatal(err)
+    }
+}
