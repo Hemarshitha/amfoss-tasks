de29d5ef808fe58340212b1191857de122c0da32
Hemarshitha <hemarshitha2006@gmail.com>
2024-08-24 00:16:21 +0530

    added cpp.3

---
+++ TASK-03/cpp/subtask3.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+int main() {
+    int n;
+    std::cout << "Enter an odd number: ";
+    std::cin >> n;
+
+    // Upper part
+    for (int i = 0; i <= n / 2; i++) {
+    }
+
+    // Lower part
+    for (int i = n / 2 - 1; i >= 0; i--) {
+    }
+    return 0;
+}
