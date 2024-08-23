a1ae9ae52b9f604ac4afe9ce9988c4918b0b0942
Hemarshitha <hemarshitha2006@gmail.com>
2024-08-23 13:39:20 +0530

    added py.2

--- TASK-03/python/subtask2.py
+++ TASK-03/python/subtask2.py
@@ -1,4 +1,4 @@
-echo 'with open("input.txt", "r") as file:' > subtask2.py
-echo '    data = file.read()' >> subtask2.py
-echo 'with open("output.txt", "w") as file:' >> subtask2.py
-echo '    file.write(data)' >> subtask2.py
+with open("input.txt", "r") as file:
+    data = file.read()
+with open("output.txt", "w") as file:
+    file.write(data)
